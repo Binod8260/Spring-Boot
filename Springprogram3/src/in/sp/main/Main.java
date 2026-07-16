277c91ae6824f04192230faf0ad99c81f660b300
Binod8260 <binoddas1143@gmail.com>
2026-07-16 23:10:29 +0530

    first commit

---
+++ Springprogram3/src/in/sp/main/Main.java
@@ -0,0 +1,21 @@
+package in.sp.main;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+import in.sp.beans.Student;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		String resources_file_path = "in/sp/resources/applicationContext.xml";
+		
+	ApplicationContext context = new ClassPathXmlApplicationContext(resources_file_path );
+	Student std = context.getBean("student",Student.class);
+	std.display();
+	}
+
+}
