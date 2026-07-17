6645e50ad716f201fc8dcb5c728620fb697c0cda
Binod8260 <binoddas1143@gmail.com>
2026-07-17 22:52:20 +0530

    first commit

---
+++ Springprogram5/src/in/sp/beans/Address.java
@@ -0,0 +1,30 @@
+package in.sp.beans;
+
+public class Address {
+	private int houseno;
+	private String city;
+	private int pincode;
+	public void setHouseno(int houseno) {
+		System.out.println("Setter method houseno");
+		this.houseno = houseno;
+	}
+	public void setCity(String city) {
+		System.out.println("Setter method city");
+		this.city = city;
+	}
+	public void setPincode(int pincode) {
+		System.out.println("Setter method pincode");
+		this.pincode = pincode;
+	}
+//	public void display() {
+//		System.out.println("Houseno:" +houseno);
+//		System.out.println("City:" +city);
+//		System.out.println("Pincode: " +pincode);
+//	}
+	@Override
+	public String toString() {
+		return "#" +houseno+", "+city+", "+pincode;
+	}
+	
+
+}
